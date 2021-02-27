09023630598c68f37b2e1219fcc912436794c754
y2k01 <stanportableuse@gmail.com>
2021-02-28 00:52:43 +0900

    horizontal flip fix

--- Player.gd
+++ Player.gd
@@ -1,47 +1,44 @@
 extends KinematicBody2D
 
-const ACCELERATION = 512
+const TARGET_FPS = 60
+const ACCELERATION = 8
 const MAX_SPEED = 64
-const FRICTION = 0.25
-const AIR_RESISTANCE = 0.02
-const GRAVITY = 200
-const JUMP_FORCE = 128
+const FRICTION = 10
+const AIR_RESISTANCE = 1
+const GRAVITY = 4
+const JUMP_FORCE = 140
 
 var motion = Vector2.ZERO
 
-onready var sprite = $Sprite
+onready var spritePlayer = $Sprite/Player
 onready var animationPlayer = $AnimationPlayer
 
 func _physics_process(delta):
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
-	if x_input != 0:	#if we are moving
+	if x_input != 0:
 		animationPlayer.play("Run")
-		
-		motion.x += x_input * ACCELERATION * delta
+		motion.x += x_input * ACCELERATION * delta * TARGET_FPS
 		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
-		if x_input < 0:
-			sprite.flip_h = true
-		else:
-			sprite.flip_h = false
+		spritePlayer.flip_h = x_input < 0
 	else:
 		animationPlayer.play("Stand")
-		
-	motion.y += GRAVITY * delta
+	
+	motion.y += GRAVITY * delta * TARGET_FPS
 	
 	if is_on_floor():
-		if x_input == 0:	#if we are not moving
-			motion.x = lerp(motion.x, 0, FRICTION)
+		if x_input == 0:
+			motion.x = lerp(motion.x, 0, FRICTION * delta)
 			
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -JUMP_FORCE
 	else:
 		animationPlayer.play("Jump")
 		
-		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE / 2:
-			motion.y = -JUMP_FORCE / 2
-			
+		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
+			motion.y = -JUMP_FORCE/2
+		
 		if x_input == 0:
-			motion.x = lerp(motion.x, 0, AIR_RESISTANCE)
+			motion.x = lerp(motion.x, 0, AIR_RESISTANCE * delta)
 	
 	motion = move_and_slide(motion, Vector2.UP)
